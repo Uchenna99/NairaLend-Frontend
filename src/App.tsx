cf1e0439bcbc1d26fd3b8ddddbb2760ca27b91c5
Uchenna99 <uchennaagbu@gmail.com>
2025-02-08 00:14:50 +0100

    setting up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './pages/LandingPage'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 
 
 function App() {
 
   return (
     <>
+        <Routes>
+          <Route path='/' element={<LandingPage/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
     </>
   )
 }
