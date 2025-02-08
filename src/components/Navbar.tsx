1c1f83d9bb22dd2a0410ac3cb1faa5f93c68ff2e
Uchenna99 <uchennaagbu@gmail.com>
2025-02-08 14:05:38 +0100

    built hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,33 @@
+import { Link } from "react-router-dom";
 
 
 
 const Navbar = () => {
   return (
     <>
-        <div className="navbar-container"></div>
+        <div className="navbar-container">
+          <div className="main-navbar">
+            <div className="main-nav-left">
+              <p>NairaLender</p>
+            </div>
+
+            <div className="main-nav-right">
+              <Link id="nav-login" to={'/login'}>
+                Login
+              </Link>
+              <Link id="nav-btn" to={'/signup'}>
+                Get a Loan
+              </Link>
+            </div>
+          </div>
+
+          <div className="tablet-navbar">
+            <div className="main-nav-left">
+              <p>NairaLender</p>
+            </div>
+            
+          </div>
+        </div>
     </>
   )
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import lady from "../assets/Images/hero_image.png"
 
 
 
@@ -7,7 +9,32 @@ const LandingPage = () => {
     <>
       <Navbar/>
 
-      
+      <div className="landing-wrapper">
+        <div className="hero-section">
+          <div className="hero-container">
+            <div className="hero-left">
+              <div className="hero-left-main">
+                <h2>Fast & Easy Loans To Suit Your Needs</h2>
+              </div>
+              <div className="hero-left-sub">
+                <p>Flexible payment options, low-interest rates and instant approval - All in one place.</p>
+              </div>
+              <div className="hero-left-buttons">
+                <Link id="hero-butn" to={''}>
+                  Loan Calculator
+                </Link>
+                <Link id="hero-butn" to={''}>
+                  Get a Loan
+                </Link>
+              </div>
+            </div>
+
+            <div className="hero-right">
+              <img id="hero-lady" src={lady} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
