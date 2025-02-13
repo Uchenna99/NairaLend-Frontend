09a47109bf6a8bfb46c128a8d27a34ca3f42a1b9
Uchenna99 <uchennaagbu@gmail.com>
2025-02-13 22:12:28 +0100

    building payment cards section

--- src/components/PaymentCards.tsx
+++ src/components/PaymentCards.tsx
@@ -2,7 +2,14 @@
 const PaymentCards = () => {
   return (
     <>
-        <div className="payment-cards-wrapper"></div>
+        <div className="payment-cards-wrapper">
+          <h4>Your registered cards</h4>
+
+          <div className="payment-cards-holder">
+            <div className="payment-card"><p><span id="plus">+</span> Add New Card</p></div>
+          </div>
+
+        </div>
     </>
   )
 }
